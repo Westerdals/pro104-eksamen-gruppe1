68296b512af7e7da5844f5b9ea71c3bb19f2cbd0
Robin <robinsoras@hotmail.com>
2020-05-21 16:37:17 +0200

    Adding files

---
+++ projects.js
@@ -0,0 +1,25 @@
+// Creates new project and stores it in localstorage
+function createNewProject(event){
+    event.preventDefault();
+    const projectName = document.querySelector("[name='projectName']").value;
+    const project = {projectName};
+    const projectList = JSON.parse(window.localStorage.getItem("projectList")) || [];
+    projectList.push(project);
+    window.localStorage.setItem("projectList", JSON.stringify(projectList));
+
+    renderProjectList();
+    event.target.reset();
+}
+
+// Makes the list of projects visible
+function renderProjectList() {
+    const projectList = JSON.parse(window.localStorage.getItem("projectList")) || [];
+    const projectListEl = document.getElementById("projectList");
+    projectListEl.innerHTML = "";
+    for(const project of projectList){
+        const projectEl = document.createElement("div");
+        const {projectName} = project;
+        projectEl.innerHTML = "<h3>" + project + "</h3>";
+        projectListEl.appendChild(projectEl);
+    }
+}
