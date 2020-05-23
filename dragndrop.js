f06ca03071b49c9958c6e3a0a237ecf70b7cadf6
qaszz <32517805+qaszz@users.noreply.github.com>
2020-05-23 19:30:35 +0200

    prosjektside

---
+++ dragndrop.js
@@ -0,0 +1,17 @@
+function allowDrop(e){
+    e.preventDefault();
+}
+function drag(e){
+    e.dataTransfer.setData("text", e.target.id);
+}
+
+function drop(e){
+    e.preventDefault();
+    const data = e.dataTransfer.getData("text");
+    e.target.appendChild(document.getElementById(data));
+    const newAssignment = {data};
+    const assignmentList = JSON.parse(window.localStorage.getItem("assignmentList")) || [];
+    assignmentList.push(newAssignment);
+    window.localStorage.setItem("assignmentList", JSON.stringify(assignmentList));
+    
+}
