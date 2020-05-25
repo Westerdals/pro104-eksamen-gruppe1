5a66e191125edc09e1e2cda5f919a274f9265df5
qaszz <32517805+qaszz@users.noreply.github.com>
2020-05-26 01:41:47 +0200

    Alt fungerer utenom drag and drop; Teammedlem blir ikke på siden etter refresh, men har blitt lagt til i localstorage

--- dragndrop.js
+++ dragndrop.js
@@ -2,20 +2,18 @@ function allowDrop(e){
     e.preventDefault();
 }
 function drag(e){
-    e.dataTransfer.setData("text", e.target.id);
+    const data = e.target;
+    e.dataTransfer.setData("text/plain", e.target.className);
     
 }
 
 function drop(e){
     e.preventDefault();
-    const data = e.dataTransfer.getData("text");
-    e.target.appendChild(document.getElementById(data));
-    const newAssignment = {data};
-    const assignmentList = JSON.parse(window.localStorage.getItem("assignmentList")) || [];
-    assignmentList.push(newAssignment);
-    window.localStorage.setItem("assignmentList", JSON.stringify(assignmentList));
-    
+    const data = e.dataTransfer.getData("text/plain");
+    e.target.appendChild(document.getElementsByClassName(data)[0]);
+    const assignedMember = {data};
+    const draggedMember = JSON.parse(window.localStorage.getItem("draggedMember")) || [];
+    draggedMember.push(assignedMember);
+    window.localStorage.setItem("draggedMember", JSON.stringify(draggedMember));
+    console.log("drop", data);
 }
-const workTaskList = JSON.parse(window.localStorage.getItem("workTaskList")) || [];   
-    const workTaskListEl = document.getElementById("workTaskList");
-    const taskContainer = document.getElementById("taskContainer");
--- projectSite.js
+++ projectSite.js
@@ -1,35 +1,37 @@
 const gridContainer = document.getElementsByClassName("gridContainer");
-var counter = 0;
+var divCounter = 0;
+var nameCounter = 0;
 var letters = /^[A-Za-z]+$/;
 function renderWorkTaskList() { 
-    counter++;
     const workTaskList = JSON.parse(window.localStorage.getItem("workTaskList")) || [];   
     const workTaskListEl = document.getElementById("workTaskList");
     const taskContainer = document.getElementById("taskContainer");
+    const taskSelect = document.getElementById("valueTask");
     workTaskListEl.innerHTML = "";  
     taskContainer.innerHTML = "";
     for (const workTask of workTaskList) {
         const workTaskEl = document.createElement("div");
-        const {task} = workTask;
-        workTaskEl.innerHTML = "<div>" + task + "</div>";
+        const taskDrop = document.createElement("option");
+        workTaskEl.innerHTML = "<div>" + workTask.task + "</div>";
         workTaskListEl.appendChild(workTaskEl);
-        
-        }
-
+        taskDrop.innerHTML = `<option> ${workTask.task}</option>`
+        taskSelect.appendChild(taskDrop);
+        };
+    
     //Lager en ny div for tasken som ble sendt inn
     for (const taskGrid of workTaskList){
         const newTaskDiv = document.createElement("div");
-        const {task} = taskGrid;
-        newTaskDiv.innerHTML = "<h3>Task: " + task + "</h3> <br> <b>Workers:</b>";
-        newTaskDiv.id = task;
+        const assignedMembers = document.createElement("div");
+        newTaskDiv.innerHTML = `<h3>Task: ${taskGrid.task} </h3> <b>Workers</b> <div id="${taskGrid.task}" class="dragDiv" ondrop="drop(event)" ondragover="allowDrop(event)"> Drag members here </div>`;
         newTaskDiv.className = "gridElement";
-        newTaskDiv.setAttribute("ondrop", "drop(event)");
-        newTaskDiv.setAttribute("ondragover", "allowDrop(event)");
         document.getElementsByClassName("taskGridContainer")[0].appendChild(newTaskDiv);
-
+        
+        
+        
         //Sletter ekstra diver som blir laget
         if(taskContainer.childElementCount > workTaskList.length){
-            for ( var i = 0; taskContainer.childElementCount - counter; i++){
+            divCounter++;
+            for ( var i = 0; taskContainer.childElementCount - divCounter; i++){
                 taskContainer.removeChild(taskContainer.lastChild);
             }
         }
@@ -61,13 +63,18 @@ function createNewTask(event) {
             function renderTeamMemberList(){
         const teamMemberList = JSON.parse(window.localStorage.getItem("teamMemberList")) || [];
         const teamMemberListEl = document.getElementById("teamMemberList");
+        const nameSelect = document.getElementById("valueTeamMember");
         teamMemberListEl.innerHTML = "";
         for (const workTeamMember of teamMemberList) {
             const workTeamMemberEl = document.createElement("div");
+            const nameDrop = document.createElement("option");
             const { teamMember } = workTeamMember;
-        //legger til en div med eventer som gjør det mulig å dragge og droppe
-        workTeamMemberEl.innerHTML = "<div id='"+teamMember+"' draggable='true' ondragstart='drag(event)'>"+ teamMember +"</div>";
-        teamMemberListEl.appendChild(workTeamMemberEl);
+            //legger til en div med eventer som gjør det mulig å dragge og droppe
+            workTeamMemberEl.innerHTML = "<div class= '"+teamMember+"' draggable='true' ondragstart='drag(event)'>"+ teamMember +"</div>";
+            teamMemberListEl.appendChild(workTeamMemberEl);
+
+            nameDrop.innerHTML = `<select>${teamMember}</select>`
+            nameSelect.appendChild(nameDrop);
         }
     }
     
@@ -83,7 +90,7 @@ function createNewTask(event) {
             window.localStorage.setItem("teamMemberList", JSON.stringify(teamMemberList));
             event.target.reset();
             renderTeamMemberList();
-            getSelectOptions();
+          //  getSelectOptions();
         }
         else{
             alert("Venligst skriv noe inn på feltet")
@@ -98,63 +105,44 @@ function createNewTask(event) {
             
     
             function submitAssignmentTask(event) {
-              event.preventDefault();
-                var option = document.getElementsByTagName("option");
-                let taskValue = document.querySelector("#valueTask").value;
-                let memberValue = document.querySelector("#valueTeamMember").value;
-                var task = document.getElementById(taskValue);
-                
-                let newAssignment = {
-                    "task": taskValue,
-                    "teamMember": memberValue
-                };
-    
-                let assignmentList = JSON.parse(window.localStorage.getItem("assignmentList")) || [];
-                assignmentList.push(newAssignment);
-                window.localStorage.setItem("assignmentList", JSON.stringify(assignmentList));
-    
-    
-                let assignmentContainer = document.getElementById("assignmentList");
-                task.innerHTML += `<div>${memberValue}</div> `;
-               event.target.reset();
+                event.preventDefault();
+                nameCounter++;
+                const name = document.querySelector("#valueTask").value;
+                const task = document.querySelector("#valueTeamMember").value;
+                const assignedTask = {name, task};
+
+                const assignedTaskList = JSON.parse(window.localStorage.getItem("assignedTaskList")) || [];
+                assignedTaskList.push(assignedTask);
+                window.localStorage.setItem("assignedTaskList", JSON.stringify(assignedTaskList));
+                renderAssignedTaskList();
+                event.target.reset();
             }
             
-        
-    
-            function getSelectOptions() {
-                let taskList = JSON.parse(window.localStorage.getItem("workTaskList"));
-                let memberList = JSON.parse(window.localStorage.getItem("teamMemberList"));
-    
-                let taskElement = document.querySelector("#valueTask");
-                let memberElement = document.querySelector("#valueTeamMember");
-    
-                console.log(taskList);
-                console.log(memberList);
-    
-                for (let item of taskList) {
-    
-                    console.log(item);
-    
-                    let newOption = document.createElement("option");
-                    newOption.innerText = item.task;
-                    newOption.value = item.task
-                    taskElement.appendChild(newOption);
-                }
-    
-                for (let item of memberList) {
-                    console.log(item);
-                    let newOption = document.createElement("option");
-                    newOption.innerText = item.teamMember;
-                    newOption.value = item.teamMember
-                    memberElement.appendChild(newOption);
+        function renderAssignedTaskList(){
+            const task = document.querySelector("#valueTask").value;
+            const assignedTaskList = JSON.parse(window.localStorage.getItem("assignedTaskList")) || [];
+            const teamMemberList = JSON.parse(window.localStorage.getItem("teamMemberList"));
+
+
+            for(const assignedTask of assignedTaskList){
+                const assignedTaskListEl = document.getElementById(assignedTask.name);
+                const assignedTaskEl = document.createElement("div");
+                assignedTaskEl.innerHTML = `<div>${assignedTask.task}</div>`;
+                assignedTaskListEl.appendChild(assignedTaskEl);
+
+                //sletter ekstra diver som blir laget
+                if (assignedTaskListEl.childElementCount > teamMemberList.length){
+                    for (var i = 0; assignedTaskListEl.childElementCount - nameCounter; i++){
+                        assignedTaskListEl.removeChild(assignedTaskListEl.firstChild);
+                    }    
                 }
             }
-    
-            getSelectOptions();
-
+            
+        }
 
             // Denne blir call'a når body'en på html fila blir load'a
             function onLoadRender() {
                 renderWorkTaskList()
                 renderTeamMemberList()
+                renderAssignedTaskList()
             }
